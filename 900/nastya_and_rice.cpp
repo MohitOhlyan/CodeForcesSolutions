4ef243d9e05c7e561d87299218008903cd761580
MohitOhlyan <mohitpb1511@gmail.com>
2021-08-10 17:41:56 +0530

    here we go again.

---
+++ 900/nastya_and_rice.cpp
@@ -0,0 +1,22 @@
+/* 
+	Author:- Mohit
+*/
+#include<bits/stdc++.h>
+using namespace std; 
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t; 
+    cin>>t;
+    while(t--){
+    	int n,a,b,c,d;
+    	cin>>n>>a>>b>>c>>d;
+    	int L = n * (a - b), R = n * (a + b);
+		if (R < c - d || c + d < L)
+		    cout << "No\n";
+		else
+		    cout << "Yes\n";
+    }
+    return 0;
+}
